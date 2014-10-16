e5b5b214a777df785466da1c8063f1633ad5858c
Pavel Karamyshev <pkaramishev@thumbtack.net>
2014-10-16 13:13:27 +0700

    Added configuration for server and client

--- src/main/java/net/thumbtack/configServer/client/ConsoleClient.java
+++ src/main/java/net/thumbtack/configServer/client/ConsoleClient.java
@@ -1,33 +1,55 @@
 package net.thumbtack.configServer.client;
 
 import net.thumbtack.configServer.thrift.ConfigService;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
 
 public class ConsoleClient {
-    private static final int PORT = 7911;
-    private static final Logger log = LoggerFactory.getLogger(ConsoleClient.class);
+    private static final XLogger LOG = XLoggerFactory.getXLogger(ConsoleClient.class);
 
     public static void main(String[] args) {
+        TTransport transport = null;
         try {
-            TTransport transport = new TSocket("localhost", PORT);
+            final XMLConfiguration config = new XMLConfiguration("client_config.xml");
+
+            transport = configureTransport(config);
             TBinaryProtocol protocol = new TBinaryProtocol(transport);
             ConfigService.Client client = new ConfigService.Client(protocol);
-            transport.open();
-            log.debug("Query: ololo");
-            String value = client.getValue("ololo");
-            log.debug("Returned: {}", value);
 
-            transport.close();
+            transport.open();
+            LOG.info("Listening started");
+
+            String value = client.getValue("ololo");
+            LOG.debug("Returned: {}", value);
         } catch (TTransportException e) {
-            e.printStackTrace();
+            LOG.catching(e);
         } catch (TException e) {
-            e.printStackTrace();
+            LOG.catching(e);
+        } catch (ConfigurationException e) {
+            LOG.catching(e);
         }
+        finally {
+            if (transport != null) {
+                transport.close();
+            }
+
+            LOG.info("Listening stopped");
+        }
+    }
+
+    private static TTransport configureTransport(XMLConfiguration config) {
+        final String url = config.getString("client.url");
+        final int port = config.getInt("client.port");
+
+        LOG.info("Configured transport to listen {} port {}", url, port);
+
+        return new TSocket(url, port);
     }
 }
--- src/main/java/net/thumbtack/configServer/server/ConsoleServer.java
+++ src/main/java/net/thumbtack/configServer/server/ConsoleServer.java
@@ -3,30 +3,52 @@ package net.thumbtack.configServer.server;
 import net.thumbtack.configServer.services.InMemoryConfigService;
 import net.thumbtack.configServer.services.LoggingConfigService;
 import net.thumbtack.configServer.thrift.ConfigService;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TTransportException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
 
 public class ConsoleServer implements Runnable {
-    private static final int PORT = 7911;
-    private static final Logger LOG = LoggerFactory.getLogger(ConsoleServer.class);
+    private static final XLogger LOG = XLoggerFactory.getXLogger(ConsoleServer.class);
 
     @Override
     public void run() {
         try {
-            TServerSocket serverTransport = new TServerSocket(PORT);
-            ConfigService.Processor processor = new ConfigService.Processor(new LoggingConfigService(new InMemoryConfigService()));
-            TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+            final XMLConfiguration config = new XMLConfiguration("server_config.xml");
+            final LoggingConfigService configService = new LoggingConfigService(new InMemoryConfigService());
 
-            LOG.info("Starting server on port {}", PORT);
+            TServer server = configureServer(config, configService);
 
             server.serve();
         } catch (TTransportException e) {
-            e.printStackTrace();
+            LOG.catching(e);
+        } catch (ConfigurationException e) {
+            LOG.catching(e);
         }
+    }
+
+    private TServer configureServer(XMLConfiguration config, LoggingConfigService configService) throws TTransportException {
+        final int port = config.getInt("server.port");
+        final int maxThreads = config.getInt("server.maxThreadsCount", 50);
+        final int minThreads = config.getInt("server.minThreadsCount", 5);
+
+        TServerSocket serverTransport = new TServerSocket(port);
+        ConfigService.Processor processor = new ConfigService.Processor(configService);
+        TServer server = new TThreadPoolServer(
+            new TThreadPoolServer.Args(serverTransport)
+                .maxWorkerThreads(maxThreads)
+                .minWorkerThreads(minThreads)
+                .processor(processor)
+        );
+        LOG.info("Starting server on port {}", port);
+
+        return server;
     }
 
     public static void main(String[] args) {
